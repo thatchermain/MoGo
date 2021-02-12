b78b2be030e7b9371ee042228686d4c285eb918b
thatchermain <sas.thatchermain@gmail.com>
2021-02-12 17:23:54 +0100

    Added Sliders for 2 sections using Swiper lib

---
+++ swiper.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper-container', {
+    // direction: 'horizontal',
+    loop: true,
+ 
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
