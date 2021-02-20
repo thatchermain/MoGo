5fd8c4cbe07d3d709d562b76f13563ba88f29e69
thatchermain <sas.thatchermain@gmail.com>
2021-02-20 12:26:40 +0100

    Fixed menu animation delay problem

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,39 +1,31 @@
 function mobile() {
-
-
     const hamburger = document.querySelector('.nav__hamburger');
     const menu = document.querySelector('.nav__list');
     const menuItem = document.querySelectorAll('.nav__item');
     const menuLink = document.querySelectorAll('.nav__link');
 
     hamburger.addEventListener('click', () => {
-        menu.classList.toggle('nav__mobile')
-
+        menu.classList.toggle('nav__mobile');
         if (menu.classList.contains('nav__mobile')) {
+            menu.style.animation = 'showMenu .4s ease-in-out';
             menuItem.forEach((item, index) => {
-                item.style.animation = `showLinks 1s ease-out forwards ${index/7}s`;
+                item.style.animation = `showLinks .4s ease-out both ${index/7 + .2}s`;
             })
         } else {
             menuItem.forEach(item => {
-                item.style.animation = ''
+                item.style.animation = '';
             })
         }
     })
-
     menuLink.forEach(link => {
         link.addEventListener('click', () => {
             menu.classList.remove('nav__mobile');
             menuItem.forEach(item => {
-                item.style.animation = ''
+                item.style.animation = '';
         })
     })
-
-
-
 })
 }
-
-
 mobile();
 
 export default mobile;
