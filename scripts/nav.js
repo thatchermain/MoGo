c83680813c8985c7ee7858392f5ca9c10866e9a0
thatchermain <sas.thatchermain@gmail.com>
2021-02-20 11:29:15 +0100

    Testing menu animation

--- scripts/nav.js
+++ scripts/nav.js
@@ -3,20 +3,36 @@ function mobile() {
 
     const hamburger = document.querySelector('.nav__hamburger');
     const menu = document.querySelector('.nav__list');
+    const menuItem = document.querySelectorAll('.nav__item');
     const menuLink = document.querySelectorAll('.nav__link');
 
     hamburger.addEventListener('click', () => {
-        menu.classList.toggle('nav__mobile--on');
-        menu.classList.toggle('nav__mobile--off');
-    });
+        menu.classList.toggle('nav__mobile')
+
+        if (menu.classList.contains('nav__mobile')) {
+            menuItem.forEach((item, index) => {
+                item.style.animation = `showLinks 1s ease-out forwards ${index/7}s`;
+            })
+        } else {
+            menuItem.forEach(item => {
+                item.style.animation = ''
+            })
+        }
+    })
+
     menuLink.forEach(link => {
         link.addEventListener('click', () => {
-            menu.classList.toggle('nav__mobile--on')
-            menu.classList.toggle('nav__mobile--off')
+            menu.classList.remove('nav__mobile');
+            menuItem.forEach(item => {
+                item.style.animation = ''
         })
     })
 
+
+
+})
 }
+
 
 mobile();
 
